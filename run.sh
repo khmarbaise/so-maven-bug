bc7cebd79ea08f32342fb2a1f9e8f6ea132f9104
Karl-Heinz Marbaise <info@soebes.de>
2015-02-12 21:12:24 +0100

    - Safe state of the script.

--- run.sh
+++ run.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
-mvn clean test#!/bin/bash
-mvn clean test#!/bin/bash
-mvn clean test#!/bin/bash
-mvn clean test#!/bin/bash
-mvn clean test#!/bin/bash
-mvn clean test#!/bin/bash
-mvn clean test#!/bin/bash
 mvn clean test
+./checksum.sh
+# Add change to file.
+git am 0001-Changed-only-the-signature-of-the-method.patch
+git diff HEAD~1
+mvn test
+./checksum.sh
+# Undo the above patch 
+git reset HEAD~1
+git co -- .
